33dd020d169da397941384999d1c9beecf6c4f69
Tom Andrews <git@tomandrews.co.uk>
2018-05-29 11:57:13 +0100

    Extract query

--- src/ui/Welcome.js
+++ src/ui/Welcome.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
+const query = gql`
+  {
+    browsers(top: 1) {
+      name
+    }
+  }
+`;
+
 export default () => (
   <div>
-    <Query
-      query={gql`
-        {
-          browsers(top: 1) {
-            name
-          }
-        }
-      `}>
+    <Query query={query}>
       {({ loading, error, data }) => {
         if (loading) return <p>Loading...</p>;
         if (error) return <p>Error</p>;
